ae572ec0576ce3a3856a8a1ff033941046232ced
Rade Pejanović <radepejanovic@gmail.com>
2024-06-12 14:17:41 +0200

    feat: Initialized Storage and Lambda Stacks

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { StorageStack } from '../lib/storage-stack';
+import { LambdaStack } from '../lib/lambda-stack';
 
 const app = new cdk.App();
 
@@ -14,5 +16,10 @@ new InfrastructureStack(app, 'Stockholm', {
 	env: { region: "eu-north-1" },
 	encryptBucket: true
 });
+
+const bucket = new StorageStack(app, 'StorageStack')
+new LambdaStack(app, 'LambdaStack', {
+	bucket: bucket.bucket
+})
 
 app.synth();
