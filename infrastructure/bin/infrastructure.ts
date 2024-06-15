439f2a8bcbbad1daff9ece10134be04685f91f74
Rade Pejanović <radepejanovic@gmail.com>
2024-06-15 11:58:36 +0200

    feat: Added TranscoderStack initialization

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,15 +4,20 @@ import * as cdk from 'aws-cdk-lib';
 import { StorageStack } from '../lib/storage-stack';
 import { LambdaStack } from '../lib/lambda-stack';
 import { SecurityStack } from '../lib/security-stack';
+import { TranscoderStack } from '../lib/transcoder-stack';
 
 const app = new cdk.App();
 
-const bucket = new StorageStack(app, 'StorageStack')
+const storage = new StorageStack(app, 'StorageStack')
 
 new LambdaStack(app, 'LambdaStack', {
-	bucket: bucket.bucket
+	bucket: storage.bucket
 })
 
 new SecurityStack(app, 'SecurityStack');
+
+new TranscoderStack(app, 'TranscoderStack', {
+	bucketName: storage.bucket.bucketName
+});
 
 app.synth();
