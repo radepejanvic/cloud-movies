1bef13914c5afdb8a9f3223f242250e6a43cceed
Rade Pejanović <radepejanovic@gmail.com>
2024-07-03 01:43:12 +0200

    feat: Added prepare metadata

--- infrastructure/lambda/presigned_upload_url.py
+++ infrastructure/lambda/presigned_upload_url.py
@@ -1,8 +1,11 @@
 import json
 import boto3 # type: ignore
 import os
+import logging
 
 s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['METADATA_TABLE'])
 
 def handler(event, context):
     # Extract parameters from the request
@@ -15,14 +18,22 @@ def handler(event, context):
 
     # Construct the S3 object key
     object_key = f"{movie_name}-{uuid}/{resolution}.mp4"
-    
+
     try:
         presigned_url = s3_client.generate_presigned_url(
             ClientMethod='put_object',
-            Params={'Bucket': bucket_name, 'Key': object_key, 'ContentType': 'video/mp4'},
-            ExpiresIn=3600  # URL expires in 1 hour
+            Params={
+                'Bucket': bucket_name,
+                'Key': object_key,
+                'ContentType': 'video/mp4',
+                },
+            ExpiresIn=3600,  # URL expires in 1 hour
         )
+
+        prepare_metadata(event)
+
     except Exception as e:
+        logging.error(e)
         return {
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
@@ -32,3 +43,24 @@ def handler(event, context):
         'statusCode': 200,
         'body': json.dumps({'upload_url': presigned_url})
     }
+
+
+def prepare_metadata(event):
+    movie_name = event['queryStringParameters']['movie_name']
+    uuid = event['queryStringParameters']['uuid']
+
+    metadata = {
+        'directory': f'{movie_name}-{uuid}',
+        'resolution': event['queryStringParameters']['resolution'],
+        'type': 'mp4',
+        'title': movie_name,
+        'description': event['queryStringParameters']['description'],
+        'actors': event['queryStringParameters']['actors'],
+        'directors': event['queryStringParameters']['directors'],
+        'genres': event['queryStringParameters']['genres'],
+        'uploaded': False
+    }
+
+    table.put_item(Item=metadata)
+    
+ 
